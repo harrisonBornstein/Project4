d925b31f24c28d1f50b9c564d2f4e166414e6fa1
harrisonBornstein <harrib@d-ip-10-192-80-60.wireless-nc2.nor.ou.edu>
2013-10-30 14:01:04 -0500

    Driver Data

--- Driver.java
+++ Driver.java
@@ -1,10 +1,16 @@
 
 public class Driver {
 
+	 static ScholarshipModel model;
+	 static ScholarshipController controller;
+	 static ScholarshipView view;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		
 		
 	}
 		
