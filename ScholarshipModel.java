0083b091b062af063b0729729aea546f14f5e83f
kevinten11 <kreimnitz@ou.edu>
2013-11-11 16:07:07 -0600

    remove Methods

--- ScholarshipModel.java
+++ ScholarshipModel.java
@@ -107,7 +107,7 @@ public class ScholarshipModel {
 			if (journal.getVolumes().size() == 0)
 			{
 				//if journal is now empty, remove it
-				journals.remove(journal);
+				removeJournal(journal);
 			}
 		}
 		
@@ -116,17 +116,115 @@ public class ScholarshipModel {
 	
 	public void removeScholar(Scholar outScholar)
 	{
+		scholars.remove(outScholar);
+		
+		//remove the scholar from papers
+		for (Paper paper: publications)
+		{
+			if (paper.getAuthors().contains(outScholar))
+			{
+				paper.getAuthors().remove(outScholar);
+			}
+			//if paper has no more authors remove it
+			if (paper.getAuthors().size() == 0)
+			{
+				removePaper(paper);
+			}
+			
+		}
+		
+		//remove the scholar from journals
+		for (Journal journal: journals)
+		{
+			for (Volume volume: journal.getVolumes())
+			{
+				for (Issue issue: volume.getIssues())
+				{
+					//remove from Editors
+					if (issue.getEditors().contains(outScholar))
+					{
+						issue.getEditors().remove(outScholar);
+					}
+					
+					//remove from reviewers
+					if (issue.getReviewers().contains(outScholar))
+					{
+						issue.getReviewers().remove(outScholar);
+					}
+					
+					//if there are no more editors or no more reviewers
+					if (issue.getEditors().size() == 0 || issue.getReviewers().size() == 0)
+					{
+						//issue is now empty, remove it
+						volume.getIssues().remove(issue);
+					}
+					
+				}
+				if (volume.getIssues().size() == 0)
+				{
+					//if volume is now empty, remove it
+					journal.getVolumes().remove(volume);
+				}
+			}
+			if (journal.getVolumes().size() == 0)
+			{
+				//if journal is now empty, remove it
+				removeJournal(journal);
+			}
+		}
+		
+		//remove the scholar from conferences
+		for (Conference conference: conferences)
+		{
+			for (Meeting meeting: conference.getMeetings())
+			{
+				if (meeting.getChairs().contains(outScholar))
+				{
+					meeting.getChairs().remove(outScholar);
+				}
+				if (meeting.getMembers().contains(outScholar))
+				{
+					meeting.getMembers().remove(outScholar);
+				}
+				if (meeting.getMembers().size() == 0 || meeting.getChairs().size() == 0)
+				{
+					//if meeting meeting has no more chairs or members, remove it
+					conference.getMeetings().remove(meeting);
+				}
+				
+			}
+			if (conference.getMeetings().size() == 0)
+			{
+				//if conference has no more meetings, remove it
+				removeConference(conference);
+			}
+		}
 		
 	}
 	
 	public void removeConference(Conference outCon)
 	{
-		
+		conferences.remove(outCon);
+		for (Scholar scholar : scholars)
+		{
+			if (scholar.getEfforts().contains(outCon))
+			{
+				scholar.getEfforts().remove(outCon);
+			}
+		}
 	}
 	
 	public void removeJournal(Journal outJournal)
 	{
+		journals.remove(outJournal);
 		
+		for(Scholar scholar : scholars)
+		{
+			if (scholar.getEfforts().contains(outJournal))
+			{
+				scholar.getEfforts().remove(outJournal);
+			}
+		}
 	}
 	
 	public void addActionListener(ActionListener listener)
