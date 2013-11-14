adc8509fd493c7e15694dd9b2234b7c95ed99c53
kevinten11 <kreimnitz@ou.edu>
2013-11-13 22:08:51 -0600

    javadoc volumes

--- Volume.java
+++ Volume.java
@@ -5,26 +5,45 @@ public class Volume {
 
 	private List<Issue> issues;
 	
+	/**
+	 * null Constructor
+	 */
 	public Volume()
 	{
 		issues = null;
 	}
 	
+	/**
+	 * Constructor
+	 * @param issues
+	 */
 	public Volume(List<Issue> issues)
 	{
 		this.issues = issues;
 	}
 	
+	/**
+	 * getter
+	 * @return List<Issue>
+	 */
 	public List<Issue> getIssues()
 	{
 		return issues;
 	}
 	
+	/**
+	 * setter
+	 * @param issues
+	 */
 	public void setIssues(List<Issue> issues)
 	{
 		this.issues = issues;
 	}
 	
+	/**
+	 * adds issue to issues
+	 * @param issue
+	 */
 	public void addToIssues(Issue issue)
 	{
 		this.issues.add(issue);
